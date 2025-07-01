afb839a7dda490847e2f69f8f93be6622c1314cf
NRCALT2 <lucasmarechal2612@gmail.com>
2025-07-01 18:54:48 +0200

    double require mongoose so i remove the second

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 require('dotenv').config();
 
